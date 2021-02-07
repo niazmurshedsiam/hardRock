48e95edd9bf85908dc541e2693e0e2bd822a0ae1
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-08 00:58:57 +0600

    add url searchtext song text show api searchtext show list song list console work next work show display work

--- app.js
+++ app.js
@@ -18,30 +18,3 @@ const searchSong = ()=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// // const searchSong = () =>{
-// //     const searchText = document.getElementById("searchField").value;
-// //     const url =` https://api.lyrics.ovh/suggest/${searchText}`
-// //     fetch(url)
-// //     .then(res => res.json())
-// //     .then(data => console.log(data));
-
-// // }
-
-// const searchSong = ()=>{
-//     const searchText = document.getElementById("searchField").value;
-//     const url = ` https://api.lyrics.ovh/suggest/${searchText}`
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => console.log(data));
-// }
