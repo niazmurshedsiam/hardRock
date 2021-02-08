1bc8f7ccd33b080ff7ddba9d758e8fa2e027847c
niazmurshedsiam <niazmurshed16@gmail.com>
2021-02-08 10:14:18 +0600

    display ui search song name show display title artist name

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ const searchSong = ()=>{
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySong(data.data))
 }
+
+const displaySong = songs =>{
+    const songContainer = document.getElementById("song-container");
+    songs.forEach(song =>{
+        const songDiv = document.createElement('div');
+        songDiv.className='single-result row align-items-center my-3 p-3';
+
+        songDiv.innerHTML = `
+       
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>
+    </div>
+    `;
+        songContainer.appendChild(songDiv);
+
+    });
+}
+
+
+
+        
+    
+
 
 
 
